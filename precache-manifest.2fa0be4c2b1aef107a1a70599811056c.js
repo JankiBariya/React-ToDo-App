498f624d60b9a44d8ed9215951f5ce395c9607d6
Janki Bariya <janki.190410107005@gmail.com>
2021-12-20 22:45:54 +0530

    Updates

---
+++ precache-manifest.2fa0be4c2b1aef107a1a70599811056c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706864bdf408c0621069ab026d268b66",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "d0f1f2f51351d34f60d0",
+    "url": "/React-ToDo-App/static/css/main.7cd48d57.chunk.css"
+  },
+  {
+    "revision": "649a375a3cee9cbbbfe6",
+    "url": "/React-ToDo-App/static/js/2.072ba179.chunk.js"
+  },
+  {
+    "revision": "d0f1f2f51351d34f60d0",
+    "url": "/React-ToDo-App/static/js/main.12fec386.chunk.js"
+  },
+  {
+    "revision": "f1d8c583e886a0a21978",
+    "url": "/React-ToDo-App/static/js/runtime~main.faef495b.js"
+  }
+]);
